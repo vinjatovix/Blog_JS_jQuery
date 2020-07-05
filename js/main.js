31e8164297d2fda933732fb0263a1671183f1eba
vinjatovix <vinjatovix@gmail.com>
2020-07-05 23:01:22 +0200

    css slide ok

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@
     'use strict';
 
     $(document).ready(function () {
-        $('.slider').bxSlider();
+        $('.slider').bxSlider({
+            mode: 'fade',
+            slideWidth: 1200,
+            adaptiveHeight: true,
+            
+        });
     });
 }());
