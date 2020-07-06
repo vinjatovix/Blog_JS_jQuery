5ca3b20ac674625f73fb40394b20808515f585cf
vinjatovix <vinjatovix@gmail.com>
2020-07-06 06:26:48 +0200

    transicion entre templates

--- js/main.js
+++ js/main.js
@@ -6,10 +6,20 @@
 
     articles.forEach((item) => {
         printArticles(item);
-
     });
 
+    //? selector de temas
     themeSelector();
+
+    //? scroll up
+    $('.goTop').click(function(e){
+        e.preventDefault();
+
+        $('html', 'body').animate({
+            scrollTop: 0
+        }, 5000);
+        return false;
+    });
         
 
 
