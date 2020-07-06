601f9adee57b5a61a1e2d29b192eb69c9130e05b
vinjatovix <vinjatovix@gmail.com>
2020-07-06 03:14:38 +0200

    articulos desde js y metodos abstraidos

---
+++ js/slider.js
@@ -0,0 +1,17 @@
+(function () {
+
+    //? Slider
+    $(document).ready(function () {
+        $('.slider').bxSlider({
+            mode: 'fade',
+            slideWidth: 1600,
+            adaptiveHeight: false,
+            speed: 500,
+            pager: false,
+            controls: false,
+            auto: true,
+            slideMargin: 0,
+        });
+    });
+
+}());
